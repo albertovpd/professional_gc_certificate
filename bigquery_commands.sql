32e7d73effa309cec23dea33fa6641ab556a663d
albertovpd <a.vargas.pina@gmail.com>
2020-04-22 19:00:03 +0200

    useful commands2

---
+++ bigquery_commands.sql
@@ -0,0 +1,38 @@
+-- CREATE ARRAYS TO HAVE MORE INFO IN JUST 1 ROW
+
+-- normal one
+SELECT
+  fullVisitorId,
+  date,
+  v2ProductName,
+  pageTitle
+  FROM `data-to-insights.ecommerce.all_sessions`
+WHERE visitId = 1501570398
+ORDER BY date
+
+-- array
+SELECT
+  fullVisitorId,
+  date,
+  ARRAY_AGG(v2ProductName) AS products_viewed,
+  ARRAY_AGG(pageTitle) AS pages_viewed
+  FROM `data-to-insights.ecommerce.all_sessions`
+WHERE visitId = 1501570398
+GROUP BY fullVisitorId, date
+ORDER BY date
+
+-- ---------------------
+-- Next, lets deduplicate the pages and products so we can see how many unique products were viewed. We'll simply add DISTINCT to our ARRAY_AGG()
+
+SELECT
+  fullVisitorId,
+  date,
+  ARRAY_AGG(DISTINCT v2ProductName) AS products_viewed,
+  ARRAY_LENGTH(ARRAY_AGG(DISTINCT v2ProductName)) AS distinct_products_viewed,
+  ARRAY_AGG(DISTINCT pageTitle) AS pages_viewed,
+  ARRAY_LENGTH(ARRAY_AGG(DISTINCT pageTitle)) AS distinct_pages_viewed
+  FROM `data-to-insights.ecommerce.all_sessions`
+WHERE visitId = 1501570398
+GROUP BY fullVisitorId, date
+ORDER BY date
+
---
+++ cloud_shell_commands.sql
@@ -0,0 +1,8 @@
+-- Ingest a new Dataset from Google Cloud Storage to Bigquery with Cloud Shell
+
+bq load \
+--source_format=CSV \
+--autodetect \
+--noreplace  \
+nyctaxi.2018trips \
+gs://cloud-training/OCBL013/nyc_tlc_yellow_trips_2018_subset_2.csv
