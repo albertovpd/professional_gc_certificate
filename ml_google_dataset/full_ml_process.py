3af76fef602cab1637cc54d5dfc3e4fe668e026d
albertovpd <a.vargas.pina@gmail.com>
2020-05-11 16:17:06 +0200

    course 5 on the way

---
+++ ml_google_dataset/full_ml_process.py
@@ -0,0 +1,165 @@
+-- What is really a hassle in Bigquery is that to check distribution of columns, you need to go to datastudio
+-- and visualize what you want. It is way slower than with Python
+
+-- explore dataset
+
+SELECT
+  EXTRACT(dayofweek
+  FROM
+    start_date) AS dayofweek,
+  AVG(duration) AS duration
+FROM
+  `bigquery-public-data`.london_bicycles.cycle_hire
+GROUP BY
+  dayofweek
+
+
+-- impact of a day of weekend and hour of day
+SELECT
+  EXTRACT(dayofweek
+  FROM
+    start_date) AS dayofweek,
+  AVG(duration) AS duration
+FROM
+  `bigquery-public-data`.london_bicycles.cycle_hire
+GROUP BY
+  dayofweek
+
+-- impact of number of london_bicycles
+
+SELECT
+  bikes_count,
+  AVG(duration) AS duration
+FROM
+  `bigquery-public-data`.london_bicycles.cycle_hire
+JOIN
+  `bigquery-public-data`.london_bicycles.cycle_stations
+ON
+  cycle_hire.start_station_name = cycle_stations.name
+GROUP BY
+  bikes_count
+
+
+-- creating a training dataset
+
+SELECT
+  duration,
+  start_station_name,
+  CAST(EXTRACT(dayofweek
+    FROM
+      start_date) AS STRING) AS dayofweek,
+  CAST(EXTRACT(hour
+    FROM
+      start_date) AS STRING) AS hourofday
+FROM
+  `bigquery-public-data`.london_bicycles.cycle_hire
+
+
+-- create a new dataset called bike_model (in EU)
+
+CREATE OR REPLACE MODEL
+  bike_model.model
+OPTIONS
+  (input_label_cols=['duration'],
+    model_type='linear_reg') AS
+SELECT
+  duration,
+  start_station_name,
+  CAST(EXTRACT(dayofweek
+    FROM
+      start_date) AS STRING) AS dayofweek,
+  CAST(EXTRACT(hour
+    FROM
+      start_date) AS STRING) AS hourofday
+FROM
+  `bigquery-public-data`.london_bicycles.cycle_hire
+
+
+-- To see some metrics related to model training, enter the following query into the BigQuery editor window:
+
+SELECT * FROM ML.EVALUATE(MODEL `bike_model.model`)
+
+
+-- You'll see the model sucks. so we'll be improving the model through feature engineering
+
+CREATE OR REPLACE MODEL
+  bike_model.model_weekday
+OPTIONS
+  (input_label_cols=['duration'],
+    model_type='linear_reg') AS
+SELECT
+  duration,
+  start_station_name,
+IF
+  (EXTRACT(dayofweek
+    FROM
+      start_date) BETWEEN 2 AND 6,
+    'weekday',
+    'weekend') AS dayofweek,
+  CAST(EXTRACT(hour
+    FROM
+      start_date) AS STRING) AS hourofday
+FROM
+  `bigquery-public-data`.london_bicycles.cycle_hire
+
+-- This model results in a mean absolute error of 967 seconds which is less than the 1026 seconds for the original model. Improvement!
+
+-- Bucketize hour of day
+-- Build a BigQuery ML model with the bucketized hour of day, and combined days of week features using the query below:
+
+CREATE OR REPLACE MODEL
+  bike_model.model_bucketized
+OPTIONS
+  (input_label_cols=['duration'],
+    model_type='linear_reg') AS
+SELECT
+  duration,
+  start_station_name,
+IF
+  (EXTRACT(dayofweek
+    FROM
+      start_date) BETWEEN 2 AND 6,
+    'weekday',
+    'weekend') AS dayofweek,
+  ML.BUCKETIZE(EXTRACT(hour
+    FROM
+      start_date),
+    [5, 10, 17]) AS hourofday
+FROM
+  `bigquery-public-data`.london_bicycles.cycle_hire
+
+-- Make predictions
+-- Build a BigQuery ML model with the TRANSFORM clause that incorporates the bucketized hour of day, and combined days of week features using the query below:
+
+CREATE OR REPLACE MODEL
+  bike_model.model_bucketized TRANSFORM(* EXCEPT(start_date),
+  IF
+    (EXTRACT(dayofweek
+      FROM
+        start_date) BETWEEN 2 AND 6,
+      'weekday',
+      'weekend') AS dayofweek,
+    ML.BUCKETIZE(EXTRACT(HOUR
+      FROM
+        start_date),
+      [5, 10, 17]) AS hourofday )
+OPTIONS
+  (input_label_cols=['duration'],
+    model_type='linear_reg') AS
+SELECT
+  duration,
+  start_station_name,
+  start_date
+FROM
+  `bigquery-public-data`.london_bicycles.cycle_hire
+
+-- With the TRANSFORM clause in place, enter this query to predict the duration of a rental from Park Lane right now (your result will vary):
+
+SELECT
+  *
+FROM
+  ML.PREDICT(MODEL bike_model.model_bucketized,
+    (
+    SELECT
+      'Park Lane , Hyde Park' AS start_station_name,
+      CURRENT_TIMESTAMP() AS start_date) )
